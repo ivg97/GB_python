59b541f17a418d8dbce378c48c6f2f6b51bd040e
ivg97 <gavr97.97@mail.ru>
2021-02-15 15:58:19 +0300

    Homework_2

---
+++ Gavrilko_Alexandr_dz_2/task_1.py
@@ -0,0 +1,11 @@
+#Task 1
+# 1. Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+print(type(15 * 3)) # int
+print(type(15 / 3)) # float
+print(type(15 // 2)) # int
+print(type(15 ** 2)) # int
